98385fadb7fc66f95b1839630b2f69ad5446097b
jan <jan@5square.de>
2017-11-21 16:10:29 +0100

    load api key from db on linux in default location

--- scripts/ApiKey.php
+++ scripts/ApiKey.php
@@ -6,12 +6,10 @@ class ApiKey {
     const API_KEY_NAME = 'admin';
     
     public function __invoke() {
-        #$db = new \SQLite3('/usr/local/zend/var/db/gui.db');
-        #$results = $db->query("SELECT HASH FROM GUI_WEBAPI_KEYS WHERE NAME='" . self::API_KEY_NAME . "'");
+        $db = new \SQLite3('/usr/local/zend/var/db/gui.db');
+        $results = $db->query("SELECT HASH FROM GUI_WEBAPI_KEYS WHERE NAME='" . self::API_KEY_NAME . "'");
         
-        #$row = $results->fetchArray();
-        
-        $row['HASH'] = '601dda8c147db4f7188e9ff36160a8c23da68652ff735fdda102d9839e378fa8';
+        $row = $results->fetchArray();
         
         return new \ZendServerWebApi\Model\ApiKey(self::API_KEY_NAME, $row['HASH']);
     }
